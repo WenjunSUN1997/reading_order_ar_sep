bf838121a5e1e4ce45f86943413af6a03368c3d3
WenjunSUN1997 <sunwenjun1997@gmail.com>
2024-10-25 16:27:00 +0200

    init

--- model/article_single_fig_model.py
+++ model/article_single_fig_model.py
@@ -9,15 +9,19 @@ class ArticleSingleFigModel(ReadingOrderModel):
         super().__init__(config)
 
     def gt_generate(self, original_gt):
-        #TODO： re-organize and flat the gt
+        length_record = []
         result = []
         gt_matrix = original_gt.squeeze(0)
         for index_1 in range(len(gt_matrix) - 1):
+            length_item = 0
             for index_2 in range(index_1 + 1, len(gt_matrix)):
                 result.append(gt_matrix[index_1, index_2])
+                length_item += 1
+
+            length_record.append(length_item)
 
         result = torch.stack(result)
-        return result
+        return result, length_record
 
     def linear_input_generate(self, all_embedding):
         result = []
@@ -27,10 +31,35 @@ class ArticleSingleFigModel(ReadingOrderModel):
 
         return torch.cat(result, dim=0)
 
-    def decode(self, output_linear):
+    def article_decode(self, output_linear, length_record):
+        def check_in(index, result):
+            for result_item in result:
+                if index in result_item:
+                    return True
+
+            return False
+
         result = []
         max_index = torch.argmax(output_linear, dim=1).detach().cpu().numpy()
-        
+        for index, length in enumerate(length_record):
+            if check_in(index, result):
+                continue
+
+            result_item = [index]
+            if index == 0:
+                prediction = max_index[:length]
+            else:
+                prediction = max_index[length[index-1]: length]
+
+            for prediction_index, prediction_item in enumerate(prediction):
+                if prediction_item == 0 or check_in(prediction_index+index+1, result):
+                    continue
+                else:
+                    result_item.append(prediction_index+index+1)
+
+            result.append(result_item)
+
+        return result
 
     def forward(self, input):
         text_emdedding = self.lang_model(input_ids=input['input_ids'].squeeze(0),
@@ -47,9 +76,9 @@ class ArticleSingleFigModel(ReadingOrderModel):
         input_to_linear = self.linear_input_generate(all_embedding)
         input_to_linear = normalize(input_to_linear)
         output_linear = self.linear(input_to_linear)
-        gt = self.gt_generate(input['article_matirx'])
-        loss = self.loss_function(output_linear, gt)
-        article_result = self.decode(output_linear)
+        gt, length_record = self.gt_generate(input['article_matirx'])
+        loss = self.loss_func(output_linear, gt)
+        article_result = self.article_decode(output_linear, length_record)
 
 
 
